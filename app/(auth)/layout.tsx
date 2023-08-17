61786a6228d8201dc1a115da4eb4519a9c4658b8
Pratiksha <pratiksha.sontakke@torinit.ca>
2023-08-18 02:30:51 +0530

    clerk authentication setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google";
+export const metadata ={
+    title: 'Threads',
+    description: 'Next.js 13 Application'
+}
+
+const inter = Inter({subsets: ["latin"]}) //applies this font across all files
+
+export default function RootLayout({children}:{children: React.ReactNode}){
+    return (
+    <ClerkProvider>
+    <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}> {children}</body>
+    </html>
+
+   </ClerkProvider>)
+}
+
+
+   
+
